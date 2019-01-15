ed8838233ebc9bee04c54f2bd2c58745d7f576b4
Mike Zazaian <mike@functionhaus.com>
2019-01-15 18:12:32 +0100

    add comments and typespecs to metadata struct

--- lib/archivist/parsers/metadata_parser.ex
+++ lib/archivist/parsers/metadata_parser.ex
@@ -1,8 +1,26 @@
 defmodule Archivist.Parsers.MetadataParser do
+  alias Archivist.Metadata
   alias Archivist.Parsers.TagParser
+  @moduledoc """
+  A module for parsing a raw metadata value into YAML, formatting its keys and
+  values, and returning a %Metadata{} struct.
+  """
 
   @datetimes = [:created_at, :published_at]
 
+  @doc """
+  Parses a raw metadata string and formats it as a struct"
+
+  ## Examples
+
+     iex> __MODULE__.parse_raw("title: The title\n author: Dave Thomas\n")
+     %Metadata{
+       title: "The title",
+       author: "Dave Thomas"
+     }
+
+  """
+  @spec parse_raw(binary()) :: Metadata.t()
   def parse_raw(raw_metadata) do
     raw_metadata
       |> YamlElixir.read_from_string
@@ -11,6 +29,8 @@ defmodule Archivist.Parsers.MetadataParser do
       |> Enum.into %Metadata{}
   end
 
+  @doc "Build a stream for converting map key strings into atoms"
+  @spec atomize_keys({:ok, %{required(String.t()) => String.t()}) :: %{required(atom()) => String.t()})
   defp atomize_keys {:ok, parsed_metadata} do
     Stream.flat_map parsed_metadata, fn {key, val} ->
       new_key = key
@@ -21,6 +41,8 @@ defmodule Archivist.Parsers.MetadataParser do
     end
   end
 
+  @doc "Formats special values (datetimes, lists) in the map."
+  @spec parse_values({:ok, %{required(atom()) => String.t()}) :: %{required(atom()) => String.t()})
   defp parse_values atomized do
     for {key, value} <- atomized, into: %{}, fn {key, value} ->
       value = cond do
