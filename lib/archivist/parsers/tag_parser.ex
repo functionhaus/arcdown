51c001b907f9a4813a1be2622332f9eca2ae3436
Mike Zazaian <mike@functionhaus.com>
2019-01-15 22:48:45 +0100

    update tag parser function name

--- lib/archivist/parsers/tag_parser.ex
+++ lib/archivist/parsers/tag_parser.ex
@@ -2,15 +2,15 @@ defmodule Archivist.Parsers.TagParser do
   @moduledoc "A module for parsing raw tag lists from a source article."
 
   @doc """
-  Formats and parses a string of tags into an atomized list.
+  Parses and formats a list of tags held in a string into an atomized list.
 
   ##  Examples
 
-      iex> __MODULE__.parse_string " Some-List  Of--tags    Great   "
+      iex> __MODULE__.parse_list " Some-List  Of--tags    Great   "
       [:some_list, :of_tags, :great]
 
   """
-  @spec parse_string(binary()) :: [atom()]
+  @spec parse_list(binary()) :: [atom()]
   def parse_list(tag_string) doc
     tag_string
       |> String.trim
