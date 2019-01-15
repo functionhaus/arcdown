3edc60c569818f05924997f193b2c470787077a2
Mike Zazaian <mike@functionhaus.com>
2019-01-15 22:35:21 +0100

    add typespecs and docs to article parser

--- lib/archivist/parsers/article_parser.ex
+++ lib/archivist/parsers/article_parser.ex
@@ -1,9 +1,17 @@
 defmodule Archivist.Parsers.ArticleParser do
+  @moduledoc """
+  Module responsible for parsing the entire content and filename of a single
+  article file and generating an %Article{} struct populated with all
+  relevant attributes and metadata
+  """
+
   alias Archivist.Article
   alias Archivist.Parsers.MetadataParser
 
   @divider_pattern = ~r/\n---\n{1,2}/
 
+  @doc "Take in a filename, split the contents, and build the %Article struct."
+  @spec parse_file(binary()) :: Article.t()
   def parse_file path do
     path
       |> File.read
@@ -11,10 +19,15 @@ defmodule Archivist.Parsers.ArticleParser do
       |> build_article
   end
 
+  @doc "Split the metdata and content portions of the target file text."
+  @spec split_parts({:ok, binary()}) :: [binary(), binary()]
   defp split_parts {:ok, file_text} do
     Regex.split(@divider_pattern, file_text, parts: 2)
   end
 
+
+  @doc "Construct a full article from the split metadata and content parts."
+  @spec build_article({:ok, binary(), binary()}) :: Article.t()
   defp build_article {:ok, metadata, content} do
     {:ok, parsed_content, _} = content
       |> String.trim
