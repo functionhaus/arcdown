d2d57c750d383e4664fbbddb54b2ef0445f1bbe3
Mike Zazaian <mike@functionhaus.com>
2019-01-16 16:18:31 +0100

    add title and author parsing for .arc headers

--- lib/archivist/parsers/metadata_parser.ex
+++ lib/archivist/parsers/metadata_parser.ex
@@ -1,48 +1,87 @@
 defmodule Archivist.Parsers.MetadataParser do
   alias Archivist.Metadata
-  alias Archivist.Parsers.TagParser
+
   @moduledoc """
   A module for parsing a raw metadata value into YAML, formatting its keys and
   values, and returning a %Metadata{} struct.
   """
 
-  @datetimes [:created_at, :published_at]
+  # @datetimes [:created_at, :published_at]
 
   @doc "Parses a raw metadata string and formats it as a struct"
   @spec parse_raw(binary()) :: Metadata.t()
-  def parse_raw(raw_metadata) do
-    raw_metadata
-      |> YamlElixir.read_from_string
-      |> atomize_keys
-      |> parse_values
-      |> Enum.into(%Metadata{})
+  def parse_raw(raw_header) do
+    {%Metadata{}, raw_header}
+      |> parse_title
+      |> parse_slug
+      |> parse_author
+      |> parse_author_email
+      |> parse_timestamps
+      |> parse_tags
+      |> parse_summary
   end
 
-  @spec atomize_keys({:ok, %{required(String.t()) => String.t()}}) :: %{required(atom()) => String.t()}
-  defp atomize_keys {:ok, parsed_metadata} do
-    Stream.transform parsed_metadata, %{}, fn {key, val}, acc ->
-      new_key = key
-        |> String.downcase
-        |> String.to_atom
-
-      Map.put acc, new_key, val
-    end
+  def parse_title({metadata, header}) do
+    title = match_one ~r/^[\w\s]+/, header
+    {Map.put(metadata, :title, title), header}
   end
 
-  @spec parse_values({:ok, %{required(atom()) => String.t()}}) :: %{required(atom()) => String.t()}
-  defp parse_values atomized do
-    Enum.reduce atomized, fn {key, val}, acc ->
-      parsed_value = cond do
-        Enum.member? @datetimes, key ->
-          DateTime.from_naive(val, "Etc/UTC")
-        key == :tags ->
-          TagParser.parse_list val
-        true ->
-          val
-      end
-
-
-      Map.put acc, key, parsed_value
-    end
+  def parse_slug({metadata, header}) do
+    slug = match_one ~r/(?<=\<)[a-z0-9\-]+(?=\>\n)/, header
+    {Map.put(metadata, :slug, slug), header}
   end
+
+  def parse_author({metadata, header}) do
+    author = match_one ~r/(?<=\nby\ )[\w\s]+/, header
+    {Map.put(metadata, :author, author), header}
+  end
+
+  def parse_author_email({metadata, header}) do
+    author_email = match_one ~r/(?<=\nby\ ).*(?=\>\n)/, header
+    {Map.put(metadata, :author_email, author_email), header}
+  end
+
+  defp match_one(pattern, string) do
+    pattern
+      |> Regex.run(string, capture: :first)
+      |> List.first
+      |> String.trim
+  end
+
+  def parse_author(_) do
+  end
+  def parse_timestamps(_) do
+  end
+  def parse_tags(_) do
+  end
+  def parse_summary(_) do
+  end
+
+#   @spec atomize_keys({:ok, %{required(String.t()) => String.t()}}) :: %{required(atom()) => String.t()}
+#   defp atomize_keys {:ok, parsed_metadata} do
+#     Stream.transform parsed_metadata, %{}, fn {key, val}, acc ->
+#       new_key = key
+#         |> String.downcase
+#         |> String.to_atom
+#
+#       Map.put acc, new_key, val
+#     end
+#   end
+#
+#   @spec parse_values({:ok, %{required(atom()) => String.t()}}) :: %{required(atom()) => String.t()}
+#   defp parse_values atomized do
+#     Enum.reduce atomized, fn {key, val}, acc ->
+#       parsed_value = cond do
+#         Enum.member? @datetimes, key ->
+#           DateTime.from_naive(val, "Etc/UTC")
+#         key == :tags ->
+#           TagParser.parse_list val
+#         true ->
+#           val
+#       end
+#
+#
+#       Map.put acc, key, parsed_value
+#     end
+#   end
 end
--- test/parsers/metadata_parser_test.exs
+++ test/parsers/metadata_parser_test.exs
@@ -3,11 +3,32 @@ defmodule MetadataParserTest do
   alias Archivist.Parsers.MetadataParser
   alias Archivist.Metadata
 
-  test "parse raw attributes" do
-    parsed = MetadataParser.parse_raw("title: The title\n author: Dave Thomas\n")
-    assert parsed == %Metadata{
-      title: "The title",
-      author: "Dave Thomas"
-    }
+  setup do
+    {:ok, header} = "test/support/mock_header.arc"
+      |> Path.relative_to_cwd
+      |> File.read
+
+    [header: header]
   end
+
+  test "parses the title", context do
+    {%Metadata{title: title},  _} = MetadataParser.parse_title {%Metadata{}, context[:header]}
+    assert title == "The Day the Earth Stood Still"
+  end
+
+  test "parses the slug", context do
+    {%Metadata{slug: slug},  _} = MetadataParser.parse_slug {%Metadata{}, context[:header]}
+    assert slug == "the-day-the-earth-stood-still"
+  end
+
+  test "parses the author", context do
+    {%Metadata{author: author},  _} = MetadataParser.parse_author {%Metadata{}, context[:header]}
+    assert author == "Mike Zazaian"
+  end
+
+  test "parses the author's email", context do
+    {%Metadata{author_email: author_email},  _} = MetadataParser.parse_author_email {%Metadata{}, context[:header]}
+    assert author_email == "mike@functionhaus.com"
+  end
+
 end
