4b80f2c33b9729c586774fc6f424ef83a0c50c4d
Mike Zazaian <mike@functionhaus.com>
2019-01-17 18:16:40 +0100

    flesh out article struct, remove metadata struct

--- lib/arcdown/article.ex
+++ lib/arcdown/article.ex
@@ -1,17 +1,19 @@
 defmodule Arcdown.Article do
   @moduledoc """
-  The core datatype for the Arcdown system. Articles are parsed and
-  populated at compile-time along with their topics, tags and authors.
+  The core datatype for the Arcdown parser. Articles are broken into header and
+  body/content parts then compiled into the %Arcdown.Article{} struct.
   """
 
   @type t :: %__MODULE__{
-    title: String.t,
-    author: String.t,
-    summary: String.t,
-    content: String.t,
-    topic: String.t,
+    title: String.t(),
+    author: String.t(),
+    email: String.t(),
+    summary: String.t(),
+    content: String.t(),
+    topic: String.t(),
+    subtopics: [String.t()],
     tags: [atom()],
-    slug: String.t,
+    slug: String.t(),
     created_at: DateTime.t,
     published_at: DateTime.t
   }
@@ -19,13 +21,14 @@ defmodule Arcdown.Article do
   defstruct [
     :title,
     :author,
+    :email,
     :summary,
     :content,
     :topic,
+    :subtopics,
     :tags,
     :slug,
     :created_at,
     :published_at
   ]
-
 end
