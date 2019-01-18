7ecf7da84d48e4e37c3273d5f95f926fccfb6de1
Mike Zazaian <mike@functionhaus.com>
2019-01-18 14:12:03 +0100

    add doctests to tag parser, tag parser test module

--- lib/arcdown/parsers/tag_parser.ex
+++ lib/arcdown/parsers/tag_parser.ex
@@ -3,6 +3,16 @@ defmodule Arcdown.Parsers.TagParser do
 
   @tag_pattern ~r/\*[\w\s\-]+\n/
 
+  @doc ~S"""
+  Extracts raw tags from an Arcdown header and converts it into a formatted
+  list of atomized tags.
+
+  ##  Examples
+
+      iex> TagParser.from_header "* Some-List\n* Of--tags\n* Great\n"
+      {:ok, [:some_list, :of_tags, :great]}
+
+  """
   def from_header(header) do
     tags = Regex.scan(@tag_pattern, header)
       |> parse_list
@@ -10,12 +20,12 @@ defmodule Arcdown.Parsers.TagParser do
     {:ok, tags}
   end
 
-  @doc """
+  @doc ~S"""
   Parses and formats a list of tags held in a string into an atomized list.
 
   ##  Examples
 
-      iex> __MODULE__.parse_list [["* Some-List"], ["* Of--tags"], ["* Great"]]
+      iex> TagParser.parse_list [["* Some-List"], ["* Of--tags"], ["* Great"]]
       [:some_list, :of_tags, :great]
 
   """
@@ -27,6 +37,18 @@ defmodule Arcdown.Parsers.TagParser do
       |> Enum.map(&(String.to_atom &1))
   end
 
+  @doc ~S"""
+  Formats a single tag.
+
+  ##  Examples
+
+      iex> TagParser.format_tag "* Some-List"
+      "some_list"
+
+      iex> TagParser.format_tag "* Of--tags"
+      "of_tags"
+
+  """
   @spec format_tag(binary()) :: binary()
   def format_tag(tag) do
     tag
---
+++ test/parsers/tag_parser_test.exs
@@ -0,0 +1,6 @@
+defmodule TagParserTest do
+  use ExUnit.Case
+  alias Arcdown.Parsers.TagParser
+  doctest Arcdown.Parsers.TagParser
+
+end
