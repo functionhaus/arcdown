11977aa524a5ea1e808808d7fbdb9e8365b34cf5
Mike Zazaian <mike@functionhaus.com>
2019-01-19 22:50:45 +0100

    move tag parser doctests to test file

--- lib/arcdown/parsers/tag_parser.ex
+++ lib/arcdown/parsers/tag_parser.ex
@@ -3,30 +3,17 @@ defmodule Arcdown.Parsers.TagParser do
 
   @tag_pattern ~r/\*[\w\s\-]+\n/
 
-  @doc ~S"""
+  @doc """
   Extracts raw tags from an Arcdown header and converts it into a formatted
   list of atomized tags.
-
-  ##  Examples
-
-      iex> TagParser.from_header "* Some-List\n* Of--tags\n* Great\n"
-      {:ok, [:some_list, :of_tags, :great]}
-
   """
+  @spec from_header(binary()) :: [atom()]
   def from_header(header) do
     tags = Regex.scan(@tag_pattern, header)
     {:ok, parse_list(tags)}
   end
 
-  @doc ~S"""
-  Parses and formats a list of tags held in a string into an atomized list.
-
-  ##  Examples
-
-      iex> TagParser.parse_list [["* Some-List"], ["* Of--tags"], ["* Great"]]
-      [:some_list, :of_tags, :great]
-
-  """
+  @doc "Parses and formats a list of tags held in a string into an atomized list."
   @spec parse_list([list()]) :: [atom()]
   def parse_list(tags) do
     tags
@@ -35,18 +22,7 @@ defmodule Arcdown.Parsers.TagParser do
       |> Enum.map(&(String.to_atom &1))
   end
 
-  @doc ~S"""
-  Formats a single tag.
-
-  ##  Examples
-
-      iex> TagParser.format_tag "* Some-List"
-      "some_list"
-
-      iex> TagParser.format_tag "* Of--tags"
-      "of_tags"
-
-  """
+  @doc "Formats a single tag."
   @spec format_tag(binary()) :: binary()
   def format_tag(tag) do
     tag
--- test/parsers/tag_parser_test.exs
+++ test/parsers/tag_parser_test.exs
@@ -3,4 +3,18 @@ defmodule TagParserTest do
   alias Arcdown.Parsers.TagParser
   doctest Arcdown.Parsers.TagParser
 
+  test "extracting raw tags from header" do
+    {:ok, tags} = TagParser.from_header "* Some-List\n* Of--tags\n* Great\n"
+    assert tags == [:some_list, :of_tags, :great]
+  end
+
+  test "parsing a list of raw tags" do
+    tags = TagParser.parse_list [["* Some-List"], ["* Of--tags"], ["* Great"]]
+    assert tags == [:some_list, :of_tags, :great]
+  end
+
+  test "formatting a single tag" do
+    assert TagParser.format_tag("* Some-List") == "some_list"
+    assert TagParser.format_tag("* Of--tags") == "of_tags"
+  end
 end
