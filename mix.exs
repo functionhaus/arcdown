44b0a08169d8fa0c62d48b0bf77d62bccd372d3e
Mike Zazaian <mike@functionhaus.com>
2019-01-20 13:38:52 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Arcdown.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "A parsing library for articles written in Arcdown (.ad) format."
 
   def project do
